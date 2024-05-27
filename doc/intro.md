bfd72e18756150e4e06fc6047c7eb0b7891c9ca1
ricadolcvuba <rcontreras@fi.uba.ar>
2024-05-27 11:04:38 -0300

    inicio

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tp_2_algo_3
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/tp_2_algo_3/core.clj
@@ -0,0 +1,7 @@
+(ns tp-2-algo-3.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
